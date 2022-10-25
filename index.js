d48250ac593f2d234c20d2f7cd60fccd0b59e48b
siamshahriar <siamshahriar99@gmail.com>
2022-10-25 09:38:52 +0600

    this is home page by default

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 
+app.use(cors());
+
+const categories = require("./data/category.json");
+const prices = require("./data/prices.json");
+
+//this is home page by default
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("BDIX VPS and VPN API running");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log("Bdix VPN server running on port", port);
 });
